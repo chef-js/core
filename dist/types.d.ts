7248ebf1a4368dda8e7220503b537e69ffd5b645
Jacek Piętal <jpi@anixe.pl>
2023-06-11 15:57:17 +0200

    feat: improve types

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,20 +1,51 @@
 /// <reference types="node" />
-export type ServerContext = {
-  to: (topic: string) => {
-    emit: (event: string, id: string, data?: any) => void;
-  };
+/**
+ * shortcut in plugin for
+ * this.to(topic).emit(event, id, data)
+ */
+export type ServerEmitEvent = (event: string, id: string, data?: any) => void;
+/**
+ * can emit events
+ */
+export type ServerToTopic = (topic: string) => {
+  emit: ServerEmitEvent;
 };
+/**
+ * the context of plugin to a topic
+ */
+export type ServerContext = {
+  to: ServerToTopic;
+};
+/**
+ * to allow without including libraries with types as dependency
+ */
 export type Property = ((...args: any[]) => any) | any | any[];
+/**
+ * to allow without including UWS as dependency
+ */
 export type UWebSocket = {
   [property: string]: Property;
 };
+/**
+ * base event
+ */
 export type Event = {
   id: string;
   event: string;
   data?: any;
 };
+/**
+ * socket is either weboscket (socket.io) or uwebsocket (uws)
+ */
 export type Socket = WebSocket | UWebSocket;
+/**
+ * each plugin is a function that takes in websocket and event as arguments
+ * and is called each time a socket emits to topic after handshake (see chat)
+ */
 export type Plugin = (websocket: Socket, event: Event) => void;
+/**
+ * each server, be it express or uws has those props
+ */
 export type Server = {
   start: (port: number) => Promise<Server>;
   config: Config;
@@ -29,15 +60,27 @@ export type Request = (
   requestOrResponse: object | any,
   next?: () => void
 ) => void;
+/**
+ * each filereader response should contain those
+ */
 export type FileReaderResponse = {
   mime: string;
   body: string | Buffer;
   status: number;
 };
+/**
+ * filereader is a function that returns a filereader response for an url
+ */
 export type FileReader = (url: string) => FileReaderResponse;
+/**
+ * filereader cache is a latermom cache with config.maxCacheSize
+ */
 export type FileReaderCache = {
   get: FileReader;
 };
+/**
+ * chef config
+ */
 export type Config = {
   port: number;
   folder: string;
@@ -54,6 +97,9 @@ export type Config = {
   };
   maxCacheSize: number;
 };
+/**
+ * chef-core consumer props
+ */
 export type CoreConsumer = {
   createServer(config: Config): Promise<Server>;
   requestHandler(fileReaderCache: FileReaderCache): Request;
