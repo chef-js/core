9a2b6cac7ef83ffa9250144c177df23cff5bcb4a
Jacek Pietal <prozi85@gmail.com>
2025-04-29 22:29:35 +0200

    fix: Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -7,17 +7,19 @@ const ssl: { key: string; cert: string } = {
   cert: getParam("cert", resolve(__dirname, "..", "ssl", "example.crt")),
 };
 
+const DEFAULT_FOLDER = ".";
+
 const DEFAULT_PORT = "3000";
 
-const DEFAULT_FOLDER = ".";
+const DEFAULT_CACHE_SIZE = "0";
 
 const config: Config = {
   // folder to static serve files
   folder: process.argv[2] || DEFAULT_FOLDER,
   // port on which the server listens
-  port: Number(getParam("port", process.env.PORT || DEFAULT_PORT)),
+  port: Number(getParam("port", DEFAULT_PORT)),
   // max cache size prevents oom, set to 0 to disable cache
-  maxCacheSize: parseInt(getParam("maxCacheSize", "128")),
+  maxCacheSize: parseInt(getParam("maxCacheSize", DEFAULT_CACHE_SIZE)),
   // this enables http/ws logs
   debug: process.argv.includes("--debug"),
   // serve index.html on 404
